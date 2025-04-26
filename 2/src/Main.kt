d5e33771cdb13053dc1c468bb3ab62bec4d06b6e
Maksim <111689537+LazerMax@users.noreply.github.com>
2025-04-26 16:09:35 +0300

    update commit 2

--- 2/src/Main.kt
+++ 2/src/Main.kt
@@ -1,7 +1,7 @@
 import kotlin.random.Random
 
 fun main() {
-    val randomNumber = Random.nextInt(1, 10) // Генерация случайного числа от 1 до 9
+    val randomNumber = Random.nextInt(1, 9) // Генерация случайного числа от 1 до 9
 
     println("Я загадал число от 1 до 9. Попробуйте угадать!")
 
@@ -9,7 +9,7 @@ fun main() {
         print("Введите ваше предположение: ")
         val playerGuess = readln().toIntOrNull()
 
-        if (playerGuess == null) {
+        if (playerGuess == null || playerGuess < 1 || playerGuess > 9) {
             println("Некорректный ввод. Пожалуйста, введите число от 1 до 9.")
             continue // Переход к следующей итерации цикла
         }
